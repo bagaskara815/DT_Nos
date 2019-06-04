d80ada25d7966742403256a5bc5577c16bf55400
HarukeyUA <inazarrusnak@gmail.com>
2019-06-04 19:56:59 +0300

    santoni: aex official

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -27,6 +27,7 @@ $(call inherit-product, vendor/aosp/common.mk)
 $(call inherit-product, device/xiaomi/santoni/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 720
+EXTENDED_BUILD_TYPE := OFFICIAL
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
